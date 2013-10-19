a4f2970779028847ce6d3df06a7c07389d65b04e
Zbigniew Siciarz <antyqjon@gmail.com>
2013-10-19 12:50:38 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='export',
+    version=__import__('export').__version__,
+    description='A Python package joke.',
+    long_description=read('README.rst'),
+    author='Zbigniew Siciarz',
+    author_email='zbigniew@siciarz.net',
+    url='http://github.com/zsiciarz/export',
+    download_url='http://pypi.python.org/pypi/export',
+    license='MIT',
+    packages=find_packages(),
+    include_package_data=True,
+    platforms='any',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
+        'Topic :: Utilities'
+    ],
+)
